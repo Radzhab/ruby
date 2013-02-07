075b45d2f689f35fed458e5e4f5e12e83e2ae990
Radzhab <mesteruh@gmail.com>
2013-02-07 20:18:11 +0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Axixa::Application.config.secret_token = 'e9ac6595fb6ccab0c83bc5e739d08eef3167a6c3999d3d70e7a2fde983ac24bd2b7bc6256fdcf73414c465f01cc6ca16f86eabcce43def3338979aab1e33b5ba'
